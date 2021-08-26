32407d5056920df9d443a495c4e0975604c86a70
Pavel Zharov <pavel@prna.ru>
2021-08-26 23:11:03 +0700

    fix: :ambulance: remove ref in PlacePopup component

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -2,25 +2,26 @@ import { useEffect, useRef, useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 export default function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
-  const [name, setName] = useState('');
-  const link = useRef('');
+  const [values, setValues] = useState('');
 
   useEffect(() => {
-    setName('');
-    link.current.value = '';
+    setValues({
+      name: '',
+      link: ''
+    });
   }, [isOpen]);
 
   const handleChange = (e) => {
-    setName(e.target.value);
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    setValues({ ...values, [name]: value });
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    onAddPlace({
-      name,
-      link: link.current.value,
-    });
+    onAddPlace(values);
   }
 
   return (
@@ -32,12 +33,12 @@ export default function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
       onClose={onClose}
       onSubmit={handleSubmit} >
       <label htmlFor="place-name-input" className="form__label">
-        <input type="text" name="name" value={name} onChange={handleChange} id="place-name-input" placeholder="Название" className="form__input"
+        <input type="text" name="name" value={values.name} onChange={handleChange} id="place-name-input" placeholder="Название" className="form__input"
           minLength="2" required />
         <span className="form__input-error form__input-error_visible place-name-input-error"></span>
       </label>
       <label htmlFor="place-link-input" className="form__label">
-        <input ref={link} type="url" name="link" id="place-link-input" placeholder="Ссылка на картинку"
+        <input type="url" name="link" value={values.link} onChange={handleChange} id="place-link-input" placeholder="Ссылка на картинку"
           className="form__input" required />
         <span className="form__input-error form__input-error_visible place-link-input-error"></span>
       </label>
